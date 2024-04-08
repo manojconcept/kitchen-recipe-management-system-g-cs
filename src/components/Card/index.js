bae947bc4f35cc4b43c258e59655df96481ee65d
manojconcept <manojveerapavu@gmail.com>
2024-04-08 15:15:42 +0530

    backup

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Card = ({ ele }) => {
+    const [bookMark, setBookMark] = useState(false)
+
+    const bookMarkHandle = (id) => {
+        setBookMark(!bookMark)
+        return (
+            console.log(bookMark ?"":id)
+        )   
+    }
+    
     return (
         <>
             <div className="card mx-auto mb-4 position-relative" style={{ width: "18rem", height: "100%" }}>
@@ -12,15 +22,8 @@ const Card = ({ ele }) => {
                     <p className="card-text mb-1">Cuisine: {ele.cuisine}</p>
                     <p className="card-text mb-1">Diet: {ele.diet}</p>
                     <div className="d-flex justify-content-center gap-2">
-                        <div onClick={() => {}} style={{ cursor: "pointer" }}>
-                            <i className="bi bi-bookmark-check fs-1"></i>
-                        </div>
-                        <div onClick={() => {  }} style={{ cursor: "pointer" }}>
-                            <i className="bi text-warning bi-bookmark-check-fill fs-1"></i>
-                        </div>
-                        <div onClick={() => {}} style={{ cursor: "pointer" }}>
-                            <i className="bi bi-search fs-1"></i>
-                        </div>
+                        <i onClick={()=>bookMarkHandle(ele._id)} type="button" className={` bi bi-bookmark-check${bookMark ? "-fill text-warning":"" } fs-1`}></i>
+                        <i type="button" className="bi bi-book-half fs-1"></i>
                     </div>
                 </div>
             </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,40 +1,58 @@
-import React from 'react';
+import { useNavigate,Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const navigation = useNavigate("");
+    
+    const handleSingup =()=>navigation("/signup")
+
     return (
         <nav style={{ position: "sticky", top: "0", zIndex: "9000" }} className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"><i class="bi bi-egg-fill text-warning"></i>Kitchen Recipe <br />Management System</a>
+                <Link to={"/"} style={{ cursor: "pointer" }} className="navbar-brand" href="#"><i class="bi bi-egg-fill text-warning"></i>Kitchen Recipe <br />Management System</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link className="nav-link active" aria-current="page" href="#">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <Link className="nav-link" href="#">Link</Link>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
+                            <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Filter
+                            </Link>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><button className="dropdown-item" href="#">Action mm</button></li>
+                                <li><Link className="dropdown-item" href="#">Another action</Link></li>
                                 <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                <li><Link className="dropdown-item" href="#">Something else here</Link></li>
+                                <li><Link className="dropdown-item" href="#">Something else here</Link></li>
+                                <li><Link className="dropdown-item" href="#">Something else here</Link></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><Link className="dropdown-item" href="#">Something else here</Link></li>
+                                <li><Link className="dropdown-item" href="#">Something else here</Link></li>
+                                <li><Link className="dropdown-item" href="#">Something else here</Link></li>
+
+
                             </ul>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                            <Link className="nav-link disabled" aria-disabled="true">Disabled</Link>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+
+                        <i type="button" class="bi bi-search fs-1 m"></i>
                     </form>
+
+                    <div className="d-flex" >
+                        <button onClick={handleSingup} className="btn btn-outline-success " >Sign up</button>
+                    </div>
+
                 </div>
             </div>
         </nav>
