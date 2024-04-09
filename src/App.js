407f4b79cf9e292db42cc901a70cd62753b925e2
manojconcept <manojveerapavu@gmail.com>
2024-04-09 11:27:43 +0530

    testing x-nav

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Routes, Route } from "react-router-dom"
 import Main from './pages/Main';
 import Signin from './pages/Log/Signin';
 import Signup from './pages/Log/Signup';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 // import Footer from './components/Footer';
 import Test from './components/test/Test';
 
 const App = () => {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/signin' element={<Signin />} />
